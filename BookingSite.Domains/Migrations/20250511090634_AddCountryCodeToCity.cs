6d8b3202e4a52abfef8211e74befc11aa73bd44c
Elodie Vandenberghe <elodie.vandenberghe@outlook.com>
2025-05-11 16:55:28 +0200

    Adding migrations

---
+++ BookingSite.Domains/Migrations/20250511090634_AddCountryCodeToCity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingSite.Domains.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountryCodeToCity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "Cities");
+        }
+    }
+}
---
+++ BookingSite.Domains/Migrations/20250511093654_RemoveCountryCode_AddDestId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingSite.Domains.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCountryCode_AddDestId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryCode",
+                table: "Cities",
+                newName: "DestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DestId",
+                table: "Cities",
+                newName: "CountryCode");
+        }
+    }
+}
